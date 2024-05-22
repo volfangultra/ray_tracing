7cea36ca6ceead0a924316f8c5abb45c11360c7d
volfangultra <128155678+volfangultra@users.noreply.github.com>
2024-05-22 20:29:40 +0200

    Add files via upload

--- klase.js
+++ klase.js
@@ -1,99 +1,280 @@
 class Ray {
-    constructor(origin, direction){
-        this.origin = origin
-        this.direction = direction
-        this.destination = add(origin, direction)
-    }
-    current_destination(t){
-        return add(this.origin, multiply(t, this.direction));
-    } 
-    reverse(){
-        return new Ray(this.destination, multiply(-1, this.direction))
-    }
-    reflect(direction){
-        return new Ray(this.destination, subtract(multiply(2*dot_product(direction, this.direction), direction), this.direction));
-    }
+  constructor(origin, direction) {
+    this.origin = origin;
+    this.direction = direction;
+    this.destination = add(origin, direction);
+  }
+  current_destination(t) {
+    return add(this.origin, multiply(t, this.direction));
+  }
+  reverse() {
+    return new Ray(this.destination, multiply(-1, this.direction));
+  }
+  reflect(direction) {
+    return new Ray(
+      this.destination,
+      subtract(
+        multiply(2 * dot_product(direction, this.direction), direction),
+        this.direction
+      )
+    );
+  }
 }
 
 class Sphere {
-    constructor(center, radius, color, specular =-1, reflective = 0) {
-      this.center = center;
-      this.radius = radius;
-      this.color = color;
-      this.specular = specular;
-      this.reflective = reflective;
+  constructor(center, radius, color, specular = -1, reflective = 0) {
+    this.center = center;
+    this.radius = radius;
+    this.color = color;
+    this.specular = specular;
+    this.reflective = reflective;
+  }
+
+  get_normal(point) {
+    var normal = subtract(point, this.center);
+    normal = multiply(1.0 / modul(normal), normal);
+    return new Ray(point, normal);
+  }
+
+  intersect(ray) {
+    var co = subtract(ray.origin, this.center);
+
+    var a = dot_product(ray.direction, ray.direction);
+    var b = 2 * dot_product(co, ray.direction);
+    var c = dot_product(co, co) - this.radius * this.radius;
+
+    var discriminant = b * b - 4 * a * c;
+    if (discriminant < 0) {
+      return [Infinity, Infinity];
     }
 
-    get_normal(point){
-        var normal = subtract(point, this.center);  
-        normal = multiply(1.0/modul(normal), normal);
-        return new Ray(point, normal);
-    }
-
-    intersect(ray){
-        var co = subtract(ray.origin, this.center);
-
-        var a = dot_product(ray.direction, ray.direction);
-        var b = 2*dot_product(co, ray.direction);
-        var c = dot_product(co, co) - this.radius*this.radius;
-    
-        var discriminant = b*b - 4*a*c;
-        if (discriminant < 0) {
-        return [Infinity, Infinity];
-        }
-    
-        var t1 = (-b + Math.sqrt(discriminant)) / (2*a);
-        var t2 = (-b - Math.sqrt(discriminant)) / (2*a);
-        return [t1, t2];
-    }
+    var t1 = (-b + Math.sqrt(discriminant)) / (2 * a);
+    var t2 = (-b - Math.sqrt(discriminant)) / (2 * a);
+    return [t1, t2];
+  }
 }
 
-AMBIENT = 0
-POINT = 1
-DIRECTIONAL = 2
+class Paraboloid {
+  constructor(center, a2, c2, size, color, specular = -1, reflective = 0) {
+    this.center = center;
+    this.a = a2;
+    this.c = c2;
+    this.color = color;
+    this.specular = specular;
+    this.reflective = reflective;
+    this.size = size;
+  }
+
+  get_normal(point) {
+    var normal = [
+      2 * (point[0] - this.center[0]),
+      -1,
+      2 * (point[2] - this.center[2]),
+    ];
+    normal = multiply(1.0 / modul(normal), normal);
+    return new Ray(point, normal);
+  }
+
+  intersect(ray) {
+    const A = ray.direction[0];
+    const B = ray.direction[1];
+    const C = ray.direction[2];
+
+    const p = this.center[0];
+    const q = this.center[1];
+    const r = this.center[2];
+
+    const x0 = ray.origin[0];
+    const y0 = ray.origin[1];
+    const z0 = ray.origin[2];
+
+    var a = (A * A) / this.a + (C * C) / this.c;
+    var b = 2 * ((x0 - p) / this.a) * A + 2 * ((z0 - r) / this.c) * C - B;
+    var c =
+      ((x0 - p) * (x0 - p)) / this.a +
+      ((z0 - r) * (z0 - r)) / this.c -
+      (y0 - q);
+
+    var discriminant = b * b - 4 * a * c;
+    if (discriminant < 0) {
+      return [Infinity, Infinity];
+    }
+
+    var t1 = (-b + Math.sqrt(discriminant)) / (2 * a);
+    var t2 = (-b - Math.sqrt(discriminant)) / (2 * a);
+    const t_min = Math.min(t1, t2);
+    const result = [A * t_min + x0, B * t_min + y0, C * t_min + z0];
+    if (dot_product(result, result) > this.size) return [Infinity, Infinity];
+    return [t1, t2];
+  }
+}
+
+class Cone {
+  constructor(center, a2, b2, c2, size, color, specular = -1, reflective = 0) {
+    this.center = center;
+    this.a = a2;
+    this.b = b2;
+    this.c = c2;
+    this.color = color;
+    this.specular = specular;
+    this.reflective = reflective;
+    this.size = size;
+  }
+
+  get_normal(point) {
+    var normal = [
+      point[0] - this.center[0],
+      point[1] - this.center[1],
+      this.center[2] - point[2],
+    ];
+    normal = multiply(1.0 / modul(normal), normal);
+    return new Ray(point, normal);
+  }
+
+  intersect(ray) {
+    const A = ray.direction[0];
+    const B = ray.direction[1];
+    const C = ray.direction[2];
+
+    const p = this.center[0];
+    const q = this.center[1];
+    const r = this.center[2];
+
+    const x0 = ray.origin[0];
+    const y0 = ray.origin[1];
+    const z0 = ray.origin[2];
+
+    var a = (A * A) / this.a - (B * B) / this.b + (C * C) / this.c;
+    var b =
+      2 *
+      (((x0 - p) / this.a) * A -
+        ((y0 - q) / this.b) * B +
+        ((z0 - r) / this.c) * C);
+    var c =
+      ((x0 - p) * (x0 - p)) / this.a -
+      ((y0 - q) * (y0 - q)) / this.b +
+      ((z0 - r) * (z0 - r)) / this.c;
+
+    var discriminant = b * b - 4 * a * c;
+    if (discriminant < 0) {
+      return [Infinity, Infinity];
+    }
+
+    var t1 = (-b + Math.sqrt(discriminant)) / (2 * a);
+    var t2 = (-b - Math.sqrt(discriminant)) / (2 * a);
+    const t_min = Math.min(t1, t2);
+    const result = [A * t_min + x0, B * t_min + y0, C * t_min + z0];
+    if (dot_product(result, result) > this.size) return [Infinity, Infinity];
+    return [t1, t2];
+  }
+}
+
+class Sadle {
+  constructor(center, a2, c2, size, color, specular = -1, reflective = 0) {
+    this.center = center;
+    this.a = a2;
+    this.c = c2;
+    this.color = color;
+    this.specular = specular;
+    this.reflective = reflective;
+    this.size = size;
+  }
+
+  get_normal(point) {
+    var normal = [
+      2 * (point[0] - this.center[0]),
+      -1,
+      -2 * (point[2] - this.center[2]),
+    ];
+    normal = multiply(1.0 / modul(normal), normal);
+    return new Ray(point, normal);
+  }
+
+  intersect(ray) {
+    const A = ray.direction[0];
+    const B = ray.direction[1];
+    const C = ray.direction[2];
+
+    const p = this.center[0];
+    const q = this.center[1];
+    const r = this.center[2];
+
+    const x0 = ray.origin[0];
+    const y0 = ray.origin[1];
+    const z0 = ray.origin[2];
+
+    var a = (A * A) / this.a - (C * C) / this.c;
+    var b = 2 * ((x0 - p) / this.a) * A - 2 * ((z0 - r) / this.c) * C - B;
+    var c =
+      ((x0 - p) * (x0 - p)) / this.a -
+      ((z0 - r) * (z0 - r)) / this.c -
+      (y0 - q);
+
+    var discriminant = b * b - 4 * a * c;
+    if (discriminant < 0) {
+      return [Infinity, Infinity];
+    }
+    var t1 = (-b + Math.sqrt(discriminant)) / (2 * a);
+    var t2 = (-b - Math.sqrt(discriminant)) / (2 * a);
+    const t_min = Math.min(t1, t2);
+    const result = [A * t_min + x0, B * t_min + y0, C * t_min + z0];
+    if (dot_product(result, result) > this.size) return [Infinity, Infinity];
+    return [t1, t2];
+  }
+}
+
+AMBIENT = 0;
+POINT = 1;
+DIRECTIONAL = 2;
 
 class Light {
-    constructor(type, color, intensity, position) {
-      this.type = type;
-      this.color = multiply(1.0/255, color)
-      this.intensity = intensity;
-      this.position = position;
-    }  
-    compute_light(view, normal, specular = -1){
-        var color = [0, 0, 0];
-        if(this.type == AMBIENT)
-            return multiply(this.intensity, this.color);
+  constructor(type, color, intensity, position) {
+    this.type = type;
+    this.color = multiply(1.0 / 255, color);
+    this.intensity = intensity;
+    this.position = position;
+  }
+  compute_light(view, normal, specular = -1) {
+    var color = [0, 0, 0];
+    if (this.type == AMBIENT) return multiply(this.intensity, this.color);
 
-        var temp, t_max;
-        if(this.type == POINT){
-            temp = subtract(this.position, view.origin);
-            t_max = 1.0;
-        }
-        if(this.type == DIRECTIONAL){
-            temp = this.position;
-            t_max = Infinity;
-        }
-
-        if(has_shadow && find_closest_object(new Ray(view.origin, temp), EPSILON, t_max))
-            return [0, 0, 0];
-        
-
-        var normal_dot_temp = dot_product(temp, normal);
-        
-        if(normal_dot_temp > 0)
-            color = add(color, multiply(normal_dot_temp / (modul(normal) * modul(temp)), this.color));
-        
-
-        
-
-        if(specular != -1){
-            var r = reflect_direction(temp, normal);
-            var r_dot_view = dot_product(r, view.direction);
-            if(r_dot_view > 0)
-                color = add(color, multiply(Math.pow(r_dot_view/(modul(r)*modul(view.direction)), specular), this.color))
-        }
-
-        return multiply(this.intensity, color);
+    var temp, t_max;
+    if (this.type == POINT) {
+      temp = subtract(this.position, view.origin);
+      t_max = 1.0;
     }
-}
+    if (this.type == DIRECTIONAL) {
+      temp = this.position;
+      t_max = Infinity;
+    }
 
+    if (
+      has_shadow &&
+      find_closest_object(new Ray(view.origin, temp), EPSILON, t_max)
+    )
+      return [0, 0, 0];
+
+    var normal_dot_temp = dot_product(temp, normal);
+
+    if (normal_dot_temp > 0)
+      color = add(
+        color,
+        multiply(normal_dot_temp / (modul(normal) * modul(temp)), this.color)
+      );
+
+    if (specular != -1) {
+      var r = reflect_direction(temp, normal);
+      var r_dot_view = dot_product(r, view.direction);
+      if (r_dot_view > 0)
+        color = add(
+          color,
+          multiply(
+            Math.pow(r_dot_view / (modul(r) * modul(view.direction)), specular),
+            this.color
+          )
+        );
+    }
+
+    return multiply(this.intensity, color);
+  }
+}
--- test.js
+++ test.js
@@ -1,132 +1,164 @@
 var viewport_size = 1;
 var projection_plane_z = 1;
 var camera_position = [0, 0, 0];
-var camera_rotation = [[1, 0, 0], [0, 1, 0], [0, 0, 1]]
+var camera_rotation = [
+  [1, 0, 0],
+  [0, 1, 0],
+  [0, 0, 1],
+];
 
 var background_color = [0, 0, 0];
-var spheres = [ new Sphere([0, -1, 3], 1, [255, 0, 0], 500, 0.2),
-                new Sphere([2, 0, 4], 1, [0, 0, 255], 500, 0.3),
-                new Sphere([-2, 0, 4], 1, [0, 255, 0], 10, 0.4),
-                new Sphere([0, -5001, 0], 5000, [255,255,0], 1000, 0.5)
-              ];
+var spheres = [
+  new Sphere([0, -1, 3], 1, [255, 0, 0], 500, 0.2),
+  new Sphere([2, 0, 4], 1, [0, 0, 255], 500, 0.2),
+  new Sphere([-2, 0, 4], 1, [0, 255, 0], 10, 0.6),
+  new Sphere([0, -5001, 0], 5000, [255, 255, 0], 1000, 0.5),
+];
+
+var objects = [
+  new Cone(
+    [-200, 50, 1000],
+    0.001,
+    0.001,
+    0.001,
+    1050000,
+    [255, 0, 255],
+    500,
+    0.2
+  ),
+  new Paraboloid([1, 0, 4], 0.1, 0.1, 25, [0, 0, 255], 500, 0.4),
+  new Sadle([-2, 0, 10], 1, 1, 90, [0, 255, 0], 500, 0.6),
+  new Sphere([0, -1, 3], 1, [255, 0, 0], 500, 0.6),
+  new Sphere([1, 0, 3], 0.1, [255, 255, 255], 500, 0.8),
+  new Sphere([-1, 1, 3], 0.1, [255, 255, 255], 500, 0.8),
+  new Sphere([0, -5001, 0], 5000, [255, 255, 0], -1, 0.2),
+];
+
+var igranje = [new Sphere([0, -1, 3], 1, [255, 0, 0], 500, 0.2)];
 
 var has_light = false;
 var has_shadow = false;
 var num_reflections = 0;
-var objects = spheres
-
 
 const EPSILON = 0.0001;
 const MOVEMENT_SPEED = 0.5;
 const ROTATION_SPEED = 20;
 
-var lights = [new Light(AMBIENT, [255, 255, 255], 0.2, null),
-			  new Light(POINT, [255, 255, 255], 0.6, [2,1,0]),
-			  new Light(DIRECTIONAL, [255, 255, 255], 0.2, [1,4,4])];
+var lights = [
+  new Light(AMBIENT, [255, 255, 255], 0.2, null),
+  new Light(POINT, [255, 255, 255], 0.6, [2, 1, 0]),
+  new Light(DIRECTIONAL, [255, 255, 255], 0.2, [1, 4, 4]),
+];
 
-
-function start(){
-	start_ray_tracer(spheres);
-	update_canvas();
+function start() {
+  start_ray_tracer(objects);
+  update_canvas();
 }
 
-
-var toggle_light = document.getElementById('toggle_light');
-var toggle_shadow = document.getElementById('toggle_shadow');
+var toggle_light = document.getElementById("toggle_light");
+var toggle_shadow = document.getElementById("toggle_shadow");
 var set_num_reflections = document.getElementById("num_reflections");
 
-var set_ambient_color = document.getElementById('ambient_light');
-var set_point_color = document.getElementById('point_light');
+var set_ambient_color = document.getElementById("ambient_light");
+var set_point_color = document.getElementById("point_light");
 var set_directional_color = document.getElementById("directional_light");
 
-var set_ambient_intensity = document.getElementById('ambient_intensity');
-var set_point_intensity = document.getElementById('point_intensity');
-var set_directional_intensity = document.getElementById("directional_intensity");
+var set_ambient_intensity = document.getElementById("ambient_intensity");
+var set_point_intensity = document.getElementById("point_intensity");
+var set_directional_intensity = document.getElementById(
+  "directional_intensity"
+);
 
-toggle_light.addEventListener('change', function() {
-    has_light = this.checked;
-    start();
+toggle_light.addEventListener("change", function () {
+  has_light = this.checked;
+  start();
 });
 
-
-toggle_shadow.addEventListener('change', function() {
-    has_shadow = this.checked;
-    start();
+toggle_shadow.addEventListener("change", function () {
+  has_shadow = this.checked;
+  start();
 });
 
-set_num_reflections.addEventListener("change", function(){
-	num_reflections = this.value;
-	start()
+set_num_reflections.addEventListener("change", function () {
+  num_reflections = this.value;
+  start();
 });
 
-set_ambient_color.addEventListener('change', function() {
-    lights[0].color = multiply(1.0/255, hex_to_rgb(this.value));
-    start();
+set_ambient_color.addEventListener("change", function () {
+  lights[0].color = multiply(1.0 / 255, hex_to_rgb(this.value));
+  start();
 });
 
-set_point_color.addEventListener('change', function() {
-    lights[1].color = multiply(1.0/255, hex_to_rgb(this.value));
-    start();
+set_point_color.addEventListener("change", function () {
+  lights[1].color = multiply(1.0 / 255, hex_to_rgb(this.value));
+  start();
 });
 
-set_directional_color.addEventListener('change', function() {
-    lights[2].color = multiply(1.0/255, hex_to_rgb(this.value));
-    start();
+set_directional_color.addEventListener("change", function () {
+  lights[2].color = multiply(1.0 / 255, hex_to_rgb(this.value));
+  start();
 });
 
-set_ambient_intensity.addEventListener('change', function() {
-    lights[0].intensity = this.value;
-    start();
+set_ambient_intensity.addEventListener("change", function () {
+  lights[0].intensity = this.value;
+  start();
 });
 
-set_point_intensity.addEventListener('change', function() {
-    lights[1].intensity = this.value;
-    start();
+set_point_intensity.addEventListener("change", function () {
+  lights[1].intensity = this.value;
+  start();
 });
 
-set_directional_intensity.addEventListener('change', function() {
-    lights[2].intensity = this.value;
-    start();
+set_directional_intensity.addEventListener("change", function () {
+  lights[2].intensity = this.value;
+  start();
 });
 
+document.addEventListener("keydown", (event) => {
+  switch (event.key) {
+    case " ":
+      camera_position[1] += MOVEMENT_SPEED;
+      break;
+    case "ArrowLeft":
+      camera_position = add(
+        camera_position,
+        multiply(-MOVEMENT_SPEED, rotate(camera_rotation, [1, 0, 0]))
+      );
+      break;
+    case "ArrowRight":
+      camera_position = add(
+        camera_position,
+        multiply(MOVEMENT_SPEED, rotate(camera_rotation, [1, 0, 0]))
+      );
+      break;
+    case "ArrowUp":
+      camera_position = add(
+        camera_position,
+        multiply(MOVEMENT_SPEED, rotate(camera_rotation, [0, 0, 1]))
+      );
+      break;
+    case "ArrowDown":
+      camera_position = add(
+        camera_position,
+        multiply(-MOVEMENT_SPEED, rotate(camera_rotation, [0, 0, 1]))
+      );
+      break;
+    case "w":
+      camera_rotation = rotate_up(camera_rotation, ROTATION_SPEED);
+      break;
+    case "a":
+      camera_rotation = rotate_left(camera_rotation, ROTATION_SPEED);
+      break;
+    case "s":
+      camera_rotation = rotate_down(camera_rotation, ROTATION_SPEED);
+      break;
+    case "d":
+      camera_rotation = rotate_right(camera_rotation, ROTATION_SPEED);
+      break;
+  }
+  if (event.shiftKey) camera_position[1] -= MOVEMENT_SPEED;
 
-
-
-document.addEventListener('keydown', (event) => {
-		switch (event.key) {
-		    case ' ':
-		        camera_position[1] += MOVEMENT_SPEED;
-		        break;
-		    case 'ArrowLeft':
-		    	camera_position = add(camera_position, multiply(-MOVEMENT_SPEED, rotate(camera_rotation, [1,0,0])));
-		        break;
-		    case 'ArrowRight':
-		    	camera_position = add(camera_position, multiply(MOVEMENT_SPEED, rotate(camera_rotation, [1,0,0])));
-		        break;
-		    case 'ArrowUp':
-		    	camera_position = add(camera_position, multiply(MOVEMENT_SPEED, rotate(camera_rotation, [0,0,1])));
-		        break;
-		    case 'ArrowDown':
-		    	camera_position = add(camera_position, multiply(-MOVEMENT_SPEED, rotate(camera_rotation, [0,0,1])));
-		        break;
-		    case 'w':
-		    	camera_rotation = rotate_up(camera_rotation, ROTATION_SPEED)
-		        break;
-		    case 'a':
-		    	camera_rotation = rotate_left(camera_rotation, ROTATION_SPEED)
-		        break;
-		    case 's':
-		    	camera_rotation = rotate_down(camera_rotation, ROTATION_SPEED)
-		        break;
-		    case 'd':
-		    	camera_rotation = rotate_right(camera_rotation, ROTATION_SPEED)
-		        break;
-		}
-		if(event.shiftKey)
-			camera_position[1] -= MOVEMENT_SPEED
-		
-	start()
+  start();
 });
 
-
-start()
+start();
